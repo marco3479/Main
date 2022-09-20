9df6e753397a61ea404b77a3e2dec4b15166ee81
marco3479 <marcomongaloiii@gmail.com>
2022-09-20 03:33:20 -0400

    email api working

--- src/pages/sendmail.ts
+++ src/pages/sendmail.ts
@@ -1,15 +1,24 @@
-import fetch from 'node-fetch';
 import { createTransport } from 'nodemailer';
+import type { contactData } from './contact.astro';
+const API_KEY = await import.meta.env.SENDGRID_API_KEY;
 
-export async function post(req: any) {
-    const data = await req.json();
+export async function post({ request }: any) {
+    const data: contactData = await request.json();
 
     const mailData = {
         from: 'contact@sintaxis.io',
         reply: 'marcomongaloiii@gmail.com',
         to: 'marcomongaloiii@gmail.com',
         subject: data.subject,
-        text: data.body
+        text: `
+        Message sent to you from contact form in your Portfolio
+
+        Name: ${data.name}
+        Email: ${data.email}
+        Organization: ${data.organization}
+        
+        ${data.body}
+        `
     }
 
     const transporter = createTransport({
@@ -18,11 +27,11 @@ export async function post(req: any) {
         secure: true,
         auth: {
             user: 'apikey',
-            pass: process.env.SENDGRID_API_KEY
+            pass: process.env.SENDGRID_API_KEY ?? API_KEY
         }
     })
 
-    const response: any = await new Promise((resolve, reject) => {
+    const response = await new Promise((resolve, reject) => {
         transporter.sendMail(mailData, (err, info) => {
             if (err) {
                 console.log(err);
@@ -34,7 +43,5 @@ export async function post(req: any) {
         })
     })
 
-    const responseData = await response.json();
-
-    return new Response(JSON.stringify(responseData), { status: 200 });
+    return new Response(JSON.stringify(response), { status: 200 });
 }
