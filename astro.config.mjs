60bfbe4840872678057e861e4948429ca29a8cd2
marco3479 <marcomongaloiii@gmail.com>
2022-09-23 04:07:59 -0400

    making site responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from '@astrojs/preact';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), image()],
   output: "server",
   adapter: vercel()
 });
