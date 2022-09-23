797161213a831a2e5e0ea70dc7be0ef6ab3df0e9
marco3479 <marcomongaloiii@gmail.com>
2022-09-23 02:35:49 -0400

    set content for projects, interests, and skills.

--- src/ThemeState.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from "nanostores";
-
-export const isLight = atom(false);
--- src/components/ThemeMode.tsx
+++ src/components/ThemeMode.tsx
@@ -1,11 +1,6 @@
 
-import { isLight } from "../ThemeState";
 import { createCookie } from "../utils/CookiesHelper";
 
-interface Props {
-    className?: string;
-
-}
 
 export default function ThemeMode (props: any) {
 
@@ -17,15 +12,9 @@ export default function ThemeMode (props: any) {
         if (html.classList.contains('dark')) {
             html.classList.remove('dark');
             createCookie('theme', 'light', 30);
-            isLight.set(true);
-            //Astro.createCookie('theme', 'light', 30)
-            //Headers.createCookie('theme', 'light', 30);
             console.log(document.cookie)
         } else {
             html.classList.add('dark');
-            isLight.set(false);
-            //Astro.request.headers.append('theme', 'dark')
-            //Astro.createCookie('theme', 'dark', 30)
             createCookie('theme', 'dark', 30);
             }
         }
